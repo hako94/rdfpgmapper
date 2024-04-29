4351e9ad4e998d7da7c1fcb452066bd5628c292b
Hannes <hako94@web.de>
2024-04-29 16:19:33 +0200

    fix bug

--- src/main/java/rdfpgmapper/Main.java
+++ src/main/java/rdfpgmapper/Main.java
@@ -12,73 +12,72 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-                System.out.println("Mit welcher Mapping Methode möchten Sie arbeiten??");
+            System.out.println("Mit welcher Mapping Methode möchten Sie arbeiten??");
 
-                System.out.println("1 - RPT-Simple");
-                System.out.println("2 - RPT-Generic");
-                System.out.println("3 - PGT-Simple");
-                System.out.println("4 - PGT-Complete");
+            System.out.println("1 - RPT-Simple");
+            System.out.println("2 - RPT-Generic");
+            System.out.println("3 - PGT-Simple");
+            System.out.println("4 - PGT-Complete");
 
-                int option = scanner.nextInt();
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+            MapperApi mapperApi = new MapperApi(option);
+
+            boolean run = true;
+            while (run) {
+                System.out.println("\nWas möchten Sie tun?");
+                System.out.println("1 - RDF-Graph importieren");
+                System.out.println("2 - RDF-Graph exportieren");
+                System.out.println("3 - Neo4j-Datenbank leeren");
+                System.out.println("4 - Mapping-Strategie wechseln");
+                System.out.println("5 - Beenden");
+
+                System.out.print("Wählen Sie eine Option: ");
+
+                option = scanner.nextInt();
                 scanner.nextLine();
 
-                MapperApi mapperApi = new MapperApi(option);
+                switch (option) {
+                    case 1:
 
-                boolean run = true;
-                while (run) {
-                    System.out.println("\nWas möchten Sie tun?");
-                    System.out.println("1 - RDF-Graph importieren");
-                    System.out.println("2 - RDF-Graph exportieren");
-                    System.out.println("3 - Neo4j-Datenbank leeren");
-                    System.out.println("4 - Mapping-Strategie wechseln");
-                    System.out.println("5 - Beenden");
+                        Pair<String, String> input = choosePathAndFormat(scanner);
 
-                    System.out.print("Wählen Sie eine Option: ");
-
-                    option = scanner.nextInt();
-                    scanner.nextLine();
-
-                    switch (option) {
-                        case 1:
-
-                            Pair<String, String> input = choosePathAndFormat(scanner);
-
-                            if (input == null) {
-                                break;
-                            }
-
-                            mapperApi.importRdf(input.getLeft(), input.getRight());
-
-                            System.out.println("RDF-Daten wurden importiert.");
-
+                        if (input == null) {
                             break;
-                        case 2:
-                            Pair<String, String> output = choosePathAndFormat(scanner);
+                        }
 
-                            if (output == null) {
-                                break;
-                            }
+                        mapperApi.importRdf(input.getLeft(), input.getRight());
 
-                            mapperApi.exportRdf(output.getLeft(), output.getRight());
+                        System.out.println("RDF-Daten wurden importiert.");
 
-                            System.out.println("RDF-Daten wurden exportiert.");
+                        break;
+                    case 2:
+                        Pair<String, String> output = choosePathAndFormat(scanner);
 
+                        if (output == null) {
                             break;
-                        case 3:
-                            mapperApi.clearDatabase();
-                            System.out.println("Alle Daten wurden gelöscht.");
+                        }
 
-                            break;
-                        case 4:
-                            run = false;
-                            break;
-                        case 5:
-                            System.out.println("Programm beendet.");
-                            return;
-                        default:
-                            System.out.println("Ungültige Option.");
-                            break;
-                    }
+                        mapperApi.exportRdf(output.getLeft(), output.getRight());
+
+                        System.out.println("RDF-Daten wurden exportiert.");
+
+                        break;
+                    case 3:
+                        mapperApi.clearDatabase();
+                        System.out.println("Alle Daten wurden gelöscht.");
+
+                        break;
+                    case 4:
+                        run = false;
+                        break;
+                    case 5:
+                        System.out.println("Programm beendet.");
+                        return;
+                    default:
+                        System.out.println("Ungültige Option.");
+                        break;
                 }
             }
         }
